b3d2fd73289aa8a4dcc6e91077c0ac32631adb05
MARCO DELPOPOLO <156794727+MarcoDelpimme@users.noreply.github.com>
2024-03-22 14:06:46 +0100

    cmt2

--- src/components/CityCard.jsx
+++ src/components/CityCard.jsx
@@ -1,19 +1,45 @@
-import { Card, CardText } from "react-bootstrap";
+import { Card, CardText, Col, Row } from "react-bootstrap";
 
 const CityCard = ({ city }) => {
   return (
     <Card id="citycard" className="text-white">
       <Card.Body>
-        <Card.Title>
-          <h1>{city.name}</h1>
-          {city.icon && <img src={`http://openweathermap.org/img/wn/${city.icon}.png`} alt="Weather Icon" />}
-        </Card.Title>
-        <Card.Text>Temperature: {city.temperature}°C</Card.Text>
-        <Card.Text>Humidity: {city.humidity}%</Card.Text>
-        <Card.Text> MAX Temperature: {city.MaxTemp}°C</Card.Text>
-        <Card.Text> MIN Temperature: {city.MinTemp}°C</Card.Text>
+        <Row align-center>
+          <Card.Title className="row align-items-center justify-content-center">
+            <h1 className="col-auto display-2"> {city.name}</h1>
+            {city.icon && (
+              <img src={`http://openweathermap.org/img/wn/${city.icon}.png`} alt="Weather Icon" className="col-2" />
+            )}
+          </Card.Title>
+          <Row className="justify-content-center gap-5">
+            <Col xs={6} id="details">
+              {" "}
+              <h6> Temperature:</h6>
+              <span>{city.temperature}°C</span>
+              <br />
+              <i class="bi bi-thermometer"></i>
+            </Col>
+            <Col xs={6} id="details">
+              <h6>Humidity:</h6> <span>{city.humidity}%</span>
+              <br />
+              <i class="bi bi-droplet"></i>
+            </Col>
+            <Col xs={6} id="details">
+              {" "}
+              <h6>MAX Temperature:</h6> <span>{city.MaxTemp}°C</span>
+              <br />
+              <i class="bi bi-thermometer-sun"></i>
+            </Col>
+            <Col xs={6} id="details">
+              {" "}
+              <h6>MIN Temperature:</h6> <span>{city.MinTemp}°C</span>
+              <br />
+              <i class="bi bi-thermometer-snow"></i>
+            </Col>
+          </Row>
 
-        <CardText></CardText>
+          <CardText></CardText>
+        </Row>
       </Card.Body>
     </Card>
   );
